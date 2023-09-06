ff33a601cb80562e3d6f64fabd2ac64d21df0ade
Night Glow <44762336+NightGlowww@users.noreply.github.com>
2023-09-06 18:50:08 +0800

    sync

---
+++ Example12.java
@@ -0,0 +1,9 @@
+public class Example12 {
+    public static void main(String[] args) {
+        int x = 1;
+        do {
+            System.out.println("x= " + x);
+            x++;
+        }while (x <= 4);
+    }
+}
